53afa92486685974a4534d1644cc47b6300156f8
Jake Graves <freakazoidjake@gmail.com>
2023-09-26 10:47:19 -0600

    Service added.

---
+++ AnnouncementsApi/Services/AnnouncementService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using AnnouncementsApi.Models;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace AnnouncementsApi.Services
+{
+	public class AnnouncementService: IAnnouncementService
+	{
+		public AnnouncementService()
+		{
+		}
+
+        public EntityEntry<Announcement> Create(Announcement model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<Announcement> GetAll(QueryParameters queryParameters)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Announcement? GetSingle(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Announcement? Update(Announcement model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
