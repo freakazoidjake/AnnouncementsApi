0513410b92ec2d8445b5338626872f9987b7ef44
Jake Graves <freakazoidjake@gmail.com>
2023-09-26 10:42:08 -0600

    We need some kind of persistence.

---
+++ AnnouncementsApi/Services/AnnouncementDbContext.cs
@@ -0,0 +1,17 @@
+﻿using AnnouncementsApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AnnouncementsApi.Services
+{
+    public class AnnouncementDbContext : DbContext
+    {
+        public AnnouncementDbContext(DbContextOptions<AnnouncementDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Announcement> Announcements { get; set; }
+    }
+}
+
+
+
