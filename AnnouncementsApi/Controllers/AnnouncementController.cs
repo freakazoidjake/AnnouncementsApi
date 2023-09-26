e87d26c84f35d997706304cb78935080930dd745
Jake Graves <freakazoidjake@gmail.com>
2023-09-26 10:38:22 -0600

    Purges WeatherForecast.

---
+++ AnnouncementsApi/Controllers/AnnouncementController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AnnouncementsApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class AnnouncementController : ControllerBase
+{
+
+    private readonly ILogger<AnnouncementController> _logger;
+
+    public AnnouncementController(ILogger<AnnouncementController> logger)
+    {
+        _logger = logger;
+    }
+
+}
+
