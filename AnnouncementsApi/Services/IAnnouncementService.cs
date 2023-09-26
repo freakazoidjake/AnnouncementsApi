49bd9baa92f72e09b1fb07b91e59642dfccad7dd
Jake Graves <freakazoidjake@gmail.com>
2023-09-26 10:46:16 -0600

    Adds an interface for our upcoming Service.

---
+++ AnnouncementsApi/Services/IAnnouncementService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using AnnouncementsApi.Models;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace AnnouncementsApi.Services
+{
+	public interface IAnnouncementService
+	{
+        Announcement? GetSingle(int id);
+        EntityEntry<Announcement> Create(Announcement model);
+        bool Delete(int id);
+        Announcement? Update(Announcement model);
+        IQueryable<Announcement> GetAll(QueryParameters queryParameters);
+        Task<int> Save();
+    }
+}
+
